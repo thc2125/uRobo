447dc3ad2e15b7d79537a672423189d7e8fc72d8
Tyrus Cukavac <thc2125@columbia.edu>
2017-12-12 17:04:39 -0500

    Changed interface to speech generator slightly

--- t2s/speech_generator.py
+++ t2s/speech_generator.py
@@ -1,12 +1,12 @@
 #from tacotron_synthesizer import Tacotron
-from t2s.dummy_synthesizer import Dummy
+from t2s.concatenative_model.concat_nn import NNConcatenator
 
-def synthesize(text, synthesizer=Dummy()):
+def synthesize(text, data_dir, model_path, Synthesizer=NNConcatenator):
     '''Speech generation function
 
     Keyword Arguments:
     text -- a string of words from which to generate audio
     '''
-    
+    synthesizer = Synthesizer(data_dir, model_path)
     return synthesizer.synthesize(text)
 
