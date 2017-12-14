969c0a7f03adaf895f1e7c3a246ee81ca8c54d2e
TyRaNT ReX <thc2125@columbia.edu>
2017-12-13 23:18:34 -0500

    Removed main method and fixed argument names to be compatible with new starter script

--- target_feat_predicter/nn.py
+++ target_feat_predicter/nn.py
@@ -7,15 +7,20 @@ import numpy as np
 
 class NN():
 
-    def __init__(self, utt_len=0, num_phones=0, num_features=0, model_dir=0, model_path=None):
-        if model_path:
+    def __init__(self, 
+                 utt_len=0, 
+                 num_phones=0, 
+                 num_features=0, 
+                 model_path='model.h5', 
+                 existing_model_path=None):
+        if existing_model_path:
             from keras.models import load_model
-            self.model=load_model(str(model_path))
+            self.model=load_model(str(existing_model_path))
         else:
             self.model=self._init_model(utt_len, num_phones, num_features)
             self.utt_len = utt_len
             self.num_phones = num_phones
-            self.model_dir = model_dir
+            self.model_path = model_path
 
     def _init_model(self, utt_len, num_phones, num_features):
         from keras.models import Model, load_model
@@ -44,13 +49,13 @@ class NN():
 
     def train(self, utterance_phones, features, epochs=20):
         from keras.callbacks import ModelCheckpoint
-        checkpoints_dirpath = Path(self.model_dir / 'checkpoints')
+        checkpoints_dirpath = Path(self.model_path.parent / 'checkpoints')
         if not checkpoints_dirpath.exists():
             checkpoints_dirpath.mkdir(parents=True)
         checkpointer = ModelCheckpoint(filepath=str(checkpoints_dirpath
                                                     / '{epoch:02d}.hdf5'))
         self.model.fit(utterance_phones, features, callbacks=[checkpointer], epochs=epochs)
-        self.model.save(str(self.model_dir / 'model.h5'))
+        self.model.save(str(self.model_path))
 
     def get_input_length(self):
         return self.model.get_layer(index=0).input_shape[1]
@@ -60,58 +65,3 @@ class NN():
 
     def predict(self,utterance_phones):
         return self.model.predict(utterance_phones)
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('train_corpus_dir', type=Path)
-    parser.add_argument('test_corpus_dir', type=Path)
-    parser.add_argument('model_dir', type=Path)
-    parser.add_argument('-e', '--epochs', type=int)
-
-
-    args=parser.parse_args() 
-
-    train_corpus_dir = args.train_corpus_dir
-    test_corpus_dir = args.test_corpus_dir
-
-    model_dir = args.model_dir
-    if not model_dir.exists():
-        model_dir.mkdir(parents=True)
-
-    utterance_phone_file = 'utt_mono_di_tri_phones.npy'
-    utterance_phone_feats_file = 'spkr_ind_mono_di_tri_phonestarget_feats_normalized.npy'
-
-    train_phones_unpad = (np.load(str(train_corpus_dir / utterance_phone_file)))
-    train_features_unpad = (np.load(str(train_corpus_dir / utterance_phone_feats_file)))
-
-    test_phones_unpad = (np.load(str(test_corpus_dir / utterance_phone_file)))
-    test_features_unpad = (np.load(str(test_corpus_dir / utterance_phone_feats_file)))
-
-    print(train_phones_unpad.shape)
-    print(test_phones_unpad.shape)
-    utt_len = max(train_phones_unpad.shape[-1], test_phones_unpad.shape[-1])
-
-    train_padding = utt_len - train_phones_unpad.shape[-1]
-    test_padding = utt_len - test_phones_unpad.shape[-1]
-
-    train_phones = np.pad(train_phones_unpad, [(0,0), (0,train_padding)], mode='constant')
-    train_features = np.pad(train_features_unpad, [(0,0), (0,train_padding), (0,0)], mode='constant')
-    print(utt_len)
-    print(train_features.shape)
-
-    test_phones = np.pad(test_phones_unpad, [(0,0), (0,test_padding)], mode='constant')
-    test_features = np.pad(test_features_unpad, [(0,0), (0,test_padding), (0,0)], mode='constant')
-
-    num_phones = np.amax(train_phones) + 1
-    num_features = train_features.shape[-1]
-    nn = NN(utt_len, num_phones, num_features, model_dir)
-
-    if args.epochs:
-        nn.train(train_phones, train_features, args.epochs)
-    else:
-        nn.train(train_phones, train_features)
-
-
-    print(nn.evaluate(test_phones, test_features))
-
