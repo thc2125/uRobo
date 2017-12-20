56ff7ad238aa6d4632a8baa707f8bffb3aa12968
TyRaNT ReX <thc2125@columbia.edu>
2017-12-19 21:32:45 -0500

    Fixes to experiment audio, urls, and generator

--- experiments/experiment_generator.py
+++ experiments/experiment_generator.py
@@ -7,17 +7,19 @@ from concatenative_model.concat_nn import NNConcatenator
 
 # Test over the 10 hour, multi-speaker data set and the ~30 minute, 1-speaker 
 # data set
-#data_dirs = [Path('data/train-clean-10-f'), Path('data/train-clean-spk39')]
+data_dirs = [Path('data/train-clean-spk39'), Path('data/train-clean-10-f')]
 #data_dirs = [Path('data/train-clean-spk39')]
-data_dirs = [Path('data/train-clean-10-f')]
+#data_dirs = [Path('data/train-clean-10-f')]
 
-model = Path('models/train-clean-10-f/model.h5')
+model = Path('models/train-clean-10-f-lin-20e/model.h5')
 
 synthesizers = []
 for data_dir in data_dirs:
     # Test when using monophones (True) and when using triphones (False)
     #for mono in (True,False):
-    for mono in (True,):
+    for mono in (True,False):
+        if mono and data_dir.name == 'train-clean-10-f':
+            continue
         print("Building " 
               + data_dir.name 
               + ("_mono" if mono else '')
@@ -31,10 +33,10 @@ output_path = Path('experiments','audio')
 if not output_path.exists():
     output_path.mkdir()
 
-with open('experiments/experiment_text.txt') as experiment_text_file:
-    line_no = 0
-    for line in experiment_text_file:
-        for synthesizer in synthesizers:
+for synthesizer in synthesizers:
+    with open('experiments/experiment_text.txt') as experiment_text_file:
+        line_no = 0
+        for line in experiment_text_file:
             filename = str(line_no)
             print("Synthesizing '" 
                   + line 
@@ -46,4 +48,4 @@ with open('experiments/experiment_text.txt') as experiment_text_file:
             filename += '.wav'
             synthesizer.synthesize(text=line, 
                                    output_path=output_path / filename)
-        line_no += 1
+            line_no += 1
