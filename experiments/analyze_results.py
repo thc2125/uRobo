b107b839579ba9b0727dacdaf079b7a162594825
TyRaNT ReX <thc2125@columbia.edu>
2017-12-21 00:46:31 -0500

    Major updates to paper and presentation

--- experiments/analyze_results.py
+++ experiments/analyze_results.py
@@ -12,11 +12,22 @@ voices = {'c': 'Control (Human)', 't10f':'10 hours, female','ts39':'Single Speak
 t10f = re.compile('t10f')
 ts39 = re.compile('ts39')
 ts39_m = re.compile('ts39_m')
+
+sent_lens = {5:9, 6:9, 7:11, 8:18, 9:20}
 def analyze(results_file):
     workers = Counter()
     humanity_scores = defaultdict(list)
     intelligibility_scores = defaultdict(list)
     smoothness_scores = defaultdict(list)
+
+    word_humanity = defaultdict(list)
+    word_int = defaultdict(list)
+    word_smooth = defaultdict(list)
+
+    sent_humanity = defaultdict(list)
+    sent_int = defaultdict(list)
+    sent_smooth = defaultdict(list)
+
     with results_file.open() as results:
         reader = csv.DictReader(results)
         for row in reader:
@@ -31,6 +42,17 @@ def analyze(results_file):
             smoothness_scores[voice].append(int(row['Answer.Smoothness']))
             workers[row['WorkerId']] += 1
 
+            if voice == 'ts39':
+                # Word-level
+                if utterance < 5:
+                    word_humanity[utterance+1].append(int(row['Answer.Human']))
+                    word_int[utterance+1].append(int(row['Answer.Intelligible']))
+                    word_smooth[utterance+1].append(int(row['Answer.Smoothness']))
+
+                else: 
+                    sent_humanity[sent_lens[utterance]].append(int(row['Answer.Human']))
+                    sent_int[sent_lens[utterance]].append(int(row['Answer.Intelligible']))
+                    sent_smooth[sent_lens[utterance]].append(int(row['Answer.Smoothness']))
 
     print(len(workers))
     print(str(sum(workers.values())/len(workers)))
@@ -75,8 +97,56 @@ def analyze(results_file):
     ax.legend()
 
     fig.tight_layout()
-    plt.savefig('experiments/results/results_graph.png')
+    plt.savefig('experiments/results/all_voices_results.png')
     #plt.show()
+
+    # Let's do words
+    fig, ax = plt.subplots()
+
+    syllables = np.array(sorted(word_humanity.keys()))
+    word_hum = np.array([sum(hum)/len(word_humanity) for comp, hum in sorted(word_humanity.items())])
+    word_int = np.array([sum(hum)/len(word_int) for comp, hum in sorted(word_int.items())])
+    word_smooth = np.array([sum(hum)/len(word_smooth) for comp, hum in sorted(word_smooth.items())])
+
+    hum, intel, smooth = ax.plot(syllables, word_hum, syllables, word_int, syllables, word_smooth)
+    plt.setp(hum, label='Humanity')
+    plt.setp(intel, label='Intelligibility')
+    plt.setp(smooth, label='Smoothness')
+
+    ax.set_xticks(syllables)
+    ax.set_title('Performance of ts39 over words with increasing complexity')
+    ax.set_xlabel('# of Syllables')
+    ax.set_ylabel('Mean Score')
+    ax.set_xticklabels(sorted(word_humanity.keys()))
+    ax.legend()
+    fig.tight_layout()
+    plt.savefig('experiments/results/ts39_word_results.png')
+    plt.show()
+
+    # Let's do sentences
+    fig, ax = plt.subplots()
+
+    words = np.array(sorted(sent_humanity.keys()))
+    sent_hum = np.array([sum(hum)/len(sent_humanity) for comp, hum in sorted(sent_humanity.items())])
+    sent_int = np.array([sum(hum)/len(sent_int) for comp, hum in sorted(sent_int.items())])
+    sent_smooth = np.array([sum(hum)/len(sent_smooth) for comp, hum in sorted(sent_smooth.items())])
+
+    hum, intel, smooth = ax.plot(words, sent_hum, words, sent_int, words, sent_smooth)
+    plt.setp(hum, label='Humanity')
+    plt.setp(intel, label='Intelligibility')
+    plt.setp(smooth, label='Smoothness')
+
+    ax.set_xticks(words)
+    ax.set_title('Performance of ts39 over sentences with increasing complexity')
+    ax.set_xlabel('# of Words')
+    ax.set_ylabel('Mean Score')
+    ax.set_xticklabels(sorted(sent_humanity.keys()))
+    ax.legend()
+    fig.tight_layout()
+    plt.savefig('experiments/results/ts39_sent_results.png')
+    plt.show()
+   
+    
 
 if __name__=='__main__':
    parser = argparse.ArgumentParser()
